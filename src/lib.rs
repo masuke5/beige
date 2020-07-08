944a129c1811ccb3088e582ce3da04387a254749
masuke5 <s.zerogoichi@gmail.com>
2020-07-08 17:35:44 +0900

    Add compiler options

--- src/lib.rs
+++ src/lib.rs
@@ -10,26 +10,74 @@ mod lexer;
 mod parser;
 
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use id::IdMap;
 
-pub fn compile(directory: impl AsRef<Path>) {
-    let directory = directory.as_ref();
+const MAIN_MODULE_FILE: &str = "main.beige";
 
-    let main_file = directory.join("main.beige");
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum OutputType {
+    Token,
+    AST,
+    TypedAST,
+    IR,
+    Assembly,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CompileOption {
+    directory: PathBuf,
+    output: OutputType,
+}
+
+impl CompileOption {
+    pub fn new(directory: impl AsRef<Path>) -> Self {
+        Self {
+            directory: directory.as_ref().to_path_buf(),
+            output: OutputType::Assembly,
+        }
+    }
+
+    pub fn output(mut self, output: OutputType) -> Self {
+        self.output = output;
+        self
+    }
+}
+
+pub fn compile(option: CompileOption) {
+    let main_file = option.directory.join(MAIN_MODULE_FILE);
     let file = IdMap::new_id(&format!("{}", main_file.display()));
-    let code = fs::read_to_string(&main_file).unwrap();
+
+    let code = match fs::read_to_string(&main_file) {
+        Ok(code) => code,
+        Err(err) => {
+            eprintln!("Unable to read file `main.beige`: {}", err);
+            return;
+        }
+    };
 
     // Lex
     let tokens = lexer::lex(file, &code);
-    for token in &tokens {
-        println!("{:<2 } {:#} {}", token.level, token.kind, token.span);
+
+    if option.output == OutputType::Token {
+        for token in &tokens {
+            println!("{:<2 } {:#} {}", token.level, token.kind, token.span);
+        }
+        return;
     }
 
     // Parse
     let module = parser::parse(tokens, file);
-    if let Some(module) = module {
-        ast::dump_module(&module, 0);
+
+    if option.output == OutputType::AST {
+        if let Some(module) = module {
+            ast::dump_module(&module, 0);
+        }
+        return;
+    }
+
+    if error::ErrorList::has_error() {
+        return;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::process::exit;
+
+use beige::{CompileOption, OutputType};
+
+fn print_help() {
+    println!("Beige Compiler v{}", env!("CARGO_PKG_VERSION"));
+    println!("usage: beige <directory> [--dump <type>]");
+}
+
 fn main() {
-    beige::compile("tmp");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        print_help();
+        exit(1);
+    }
+
+    let directory = &args[1];
+    let output = match args.get(2).map(|s| s.as_ref()) {
+        Some("token") => OutputType::Token,
+        Some("ast") => OutputType::AST,
+        Some("ast2") => OutputType::TypedAST,
+        Some("ir") => OutputType::IR,
+        Some(s) => {
+            eprintln!("Unknown dump type: `{}`", s);
+            exit(1);
+        }
+        None => OutputType::Assembly,
+    };
+
+    let option = CompileOption::new(directory).output(output);
+    beige::compile(option);
 }
