681e213e3c407a2d30df4a6838b0e90a40e7df08
masuke5 <s.zerogoichi@gmail.com>
2020-07-15 00:24:06 +0900

    Add unify function

--- src/ty.rs
+++ src/ty.rs
@@ -64,6 +64,11 @@ impl TypePool {
 }
 
 #[derive(Debug, Clone, PartialEq)]
+pub enum TypeError {
+    Mismatch(Type, Type),
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum Type {
     App(TypeCon, Vec<Type>),
     Var(TypeVar),
@@ -82,7 +87,7 @@ pub enum TypeCon {
     Tuple,
     Record(Vec<Id>),
     TyFun(Vec<TypeVar>, Box<Type>),
-    Unique(Unique),
+    Unique(Box<TypeCon>, Unique),
 }
 
 pub fn subst(pool: &mut TypePool, ty: Type, map: &FxHashMap<TypeVar, Type>) -> Type {
@@ -118,9 +123,83 @@ pub fn subst(pool: &mut TypePool, ty: Type, map: &FxHashMap<TypeVar, Type>) -> T
     }
 }
 
+pub fn unify(pool: &mut TypePool, a: Type, b: Type) -> Result<Type, TypeError> {
+    type T = Type;
+    type C = TypeCon;
+
+    match (a, b) {
+        (T::App(C::TyFun(params, body), args), b) => {
+            // Remove TyFun
+            let new_map: FxHashMap<TypeVar, Type> =
+                params.into_iter().zip(args.into_iter()).collect();
+            let a = subst(pool, *body, &new_map);
+            unify(pool, a, b)
+        }
+        (a, T::App(C::TyFun(params, body), args)) => {
+            // Remove TyFun
+            let new_map: FxHashMap<TypeVar, Type> =
+                params.into_iter().zip(args.into_iter()).collect();
+            let b = subst(pool, *body, &new_map);
+            unify(pool, a, b)
+        }
+        (T::App(C::Unique(tycon, au), at), T::App(C::Unique(tycon_b, bu), bt)) => {
+            if au != bu {
+                // Return error if uniques are different, though the same tycon.
+                Err(TypeError::Mismatch(
+                    T::App(C::Unique(tycon, au), at),
+                    T::App(C::Unique(tycon_b, bu), bt),
+                ))
+            } else {
+                let types: Result<Vec<Type>, TypeError> = at
+                    .into_iter()
+                    .zip(bt)
+                    .map(|(a, b)| unify(pool, a, b))
+                    .collect();
+                Ok(T::App(C::Unique(tycon, au), types?))
+            }
+        }
+        (T::App(ac, at), T::App(bc, bt)) if ac == bc => {
+            // Check manually to return a correct error
+            for (at, bt) in at.iter().zip(bt.iter()) {
+                if at != bt {
+                    return Err(TypeError::Mismatch(at.clone(), bt.clone()));
+                }
+            }
+
+            let types: Result<Vec<Type>, TypeError> = at
+                .into_iter()
+                .zip(bt)
+                .map(|(a, b)| unify(pool, a, b))
+                .collect();
+            Ok(T::App(ac, types?))
+        }
+        (T::Poly(ap, at), T::Poly(bp, bt)) => {
+            let map: FxHashMap<TypeVar, Type> = bp
+                .into_iter()
+                .zip(ap.iter().map(|var| T::Var(*var)))
+                .collect();
+            let bt = subst(pool, *bt, &map);
+            Ok(T::Poly(ap, Box::new(unify(pool, *at, bt)?)))
+        }
+        (T::Var(av), T::Var(bv)) if av == bv => Ok(T::Var(av)),
+        (a, b) => Err(TypeError::Mismatch(a, b)),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    type T = Type;
+    type C = TypeCon;
+
+    fn int() -> Type {
+        T::App(C::Int, vec![])
+    }
+
+    fn bool() -> Type {
+        T::App(C::Bool, vec![])
+    }
 
     #[test]
     fn subst_with_tyfun() {
@@ -166,23 +245,102 @@ mod tests {
 
     #[test]
     fn subst_with_poly() {
-        use Type::*;
-        use TypeCon::*;
-
         let mut pool = TypePool::new();
         let var_a = pool.new_tyvar();
 
         let mut map = FxHashMap::default();
-        map.insert(var_a, App(Bool, vec![]));
+        map.insert(var_a, T::App(C::Bool, vec![]));
 
-        let ty = Poly(vec![var_a], Box::new(App(Tuple, vec![Var(var_a)])));
+        let ty = T::Poly(vec![var_a], Box::new(T::App(C::Tuple, vec![T::Var(var_a)])));
 
-        let expected = Poly(
+        let expected = T::Poly(
             vec![TypeVar(1)],
-            Box::new(App(Tuple, vec![Var(TypeVar(1))])),
+            Box::new(T::App(C::Tuple, vec![T::Var(TypeVar(1))])),
         );
         let actual = subst(&mut pool, ty, &map);
 
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn unify_simple() {
+        let mut pool = TypePool::new();
+        let var = pool.new_tyvar();
+        let uniq = pool.new_unique();
+
+        // int, int => int
+        assert_eq!(Ok(int()), unify(&mut pool, int(), int()));
+        // (int, bool), (int, int) => error
+        assert_eq!(
+            Err(TypeError::Mismatch(bool(), int())),
+            unify(
+                &mut pool,
+                T::App(C::Tuple, vec![int(), bool()]),
+                T::App(C::Tuple, vec![int(), int()])
+            ),
+        );
+        // var, var => var
+        assert_eq!(Ok(T::Var(var)), unify(&mut pool, T::Var(var), T::Var(var)));
+        // uniq, uniq => uniq
+        assert!(unify(
+            &mut pool,
+            T::App(C::Unique(Box::new(C::Int), uniq), vec![]),
+            T::App(C::Unique(Box::new(C::Bool), uniq), vec![]),
+        )
+        .is_ok())
+    }
+
+    #[test]
+    fn unify_with_tyfun() {
+        let mut pool = TypePool::new();
+        let v1 = pool.new_tyvar();
+        let v2 = pool.new_tyvar();
+
+        // (fun(v1) -> List<v1>)(bool), (fun(v2) -> List<v2>)(bool) => List<bool>
+        assert_eq!(
+            Ok(T::App(C::List, vec![bool()])),
+            unify(
+                &mut pool,
+                T::App(
+                    C::TyFun(vec![v1], Box::new(T::App(C::List, vec![T::Var(v1)]))),
+                    vec![bool()],
+                ),
+                T::App(
+                    C::TyFun(vec![v2], Box::new(T::App(C::List, vec![T::Var(v2)]))),
+                    vec![bool()],
+                ),
+            )
+        );
+    }
+
+    #[test]
+    fn unify_with_poly() {
+        let mut pool = TypePool::new();
+        let v1 = pool.new_tyvar();
+        let v2 = pool.new_tyvar();
+        let v3 = pool.new_tyvar();
+
+        // poly<v1>(List<v1>), poly<v2>(List<v2>) -> poly<v1>(List<v1>)
+        assert_eq!(
+            Ok(T::Poly(
+                vec![v1],
+                Box::new(T::App(C::List, vec![T::Var(v1)]))
+            )),
+            unify(
+                &mut pool,
+                T::Poly(vec![v1], Box::new(T::App(C::List, vec![T::Var(v1)]))),
+                T::Poly(vec![v2], Box::new(T::App(C::List, vec![T::Var(v2)]))),
+            )
+        );
+
+        // poly<v1>(List<v1>), poly<v2>(List<v3>) -> error
+        assert_eq!(
+            Err(TypeError::Mismatch(Type::Var(v1), Type::Var(v3))),
+            unify(
+                &mut pool,
+                T::Poly(vec![v1], Box::new(T::App(C::List, vec![T::Var(v1)]))),
+                T::Poly(vec![v2], Box::new(T::App(C::List, vec![T::Var(v3)]))),
+            )
+        );
     }
 }
