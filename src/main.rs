e6f0f7476d7fbfd77c437e362ccdc2c50d399038
masuke5 <s.zerogoichi@gmail.com>
2020-07-17 23:30:36 +0900

    Use clap

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,42 @@
-use std::env;
-use std::process::exit;
+use std::path::PathBuf;
+use std::str::FromStr;
 
 use beige::{CompileOption, OutputType};
+use clap::Clap;
 
-fn print_help() {
-    println!("Beige Compiler v{}", env!("CARGO_PKG_VERSION"));
-    println!("usage: beige <directory> [--dump <type>]");
+struct OutputTypeFromStr {
+    inner: OutputType,
+}
+
+impl FromStr for OutputTypeFromStr {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let ty = match s {
+            "token" => OutputType::Token,
+            "ast" => OutputType::AST,
+            "ast2" => OutputType::TypedAST,
+            "ir" => OutputType::IR,
+            s => return Err(format!("unknown dump type: `{}`", s)),
+        };
+
+        Ok(OutputTypeFromStr { inner: ty })
+    }
+}
+
+#[derive(Clap)]
+#[clap(author, about, version)]
+struct Opts {
+    #[clap(short, long)]
+    dump: Option<OutputTypeFromStr>,
+    #[clap(name = "DIRECTORY", parse(from_os_str))]
+    directory: PathBuf,
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        print_help();
-        exit(1);
-    }
+    let opt = Opts::parse();
+    let dump = opt.dump.map(|d| d.inner).unwrap_or(OutputType::Assembly);
 
-    let directory = &args[1];
-    let output = match args.get(2).map(|s| s.as_ref()) {
-        Some("token") => OutputType::Token,
-        Some("ast") => OutputType::AST,
-        Some("ast2") => OutputType::TypedAST,
-        Some("ir") => OutputType::IR,
-        Some(s) => {
-            eprintln!("Unknown dump type: `{}`", s);
-            exit(1);
-        }
-        None => OutputType::Assembly,
-    };
-
-    let option = CompileOption::new(directory).output(output);
+    let option = CompileOption::new(opt.directory).output(dump);
     beige::compile(option);
 }
